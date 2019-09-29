46c79bbfc417f601e6c651f1597e3d1a14994810
sherminebok <55948530+sherminebok@users.noreply.github.com>
2019-09-29 20:26:08 +0800

    Update solution_a.js

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,16 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+var sum_to_n = function(n) 
+{
+    int value = Integer.parseInt(n);
+	int total;
+	int sum = 0;
+	   
+	for(total=1; total<=value; total++)
+	{
+		sum += total;
+	}
+    
+	return sum;
 };
